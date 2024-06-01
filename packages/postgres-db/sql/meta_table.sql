3eefa64be6afd63d97801ab293f4c7ea11e43f27
Jim Lloyd <jim.lloyd@gmail.com>
2024-06-01 13:25:54 -0700

    memento-db: constrain meta column `kind` to the allowed kinds

--- packages/postgres-db/sql/meta_table.sql
+++ packages/postgres-db/sql/meta_table.sql
@@ -1,14 +1,14 @@
 -- The meta table is much like an index for all of the mem records.
 -- It stores metadata about the mem records, such as their kind, source, etc.
 -- The `memento` view joins the `meta` and `mem` tables to provide the logical view of the memento records.
--- The 'kind' column is a string that specifies the kind of mem record and is the desriminator key
+-- The 'kind' column is a varchar(4) string that specifies the kind of mem record and is the discriminator key
 -- for a discriminated union -- see mementoSchema.ts for the details.
 -- By using a discriminated union, we can store all of the different kinds of memento records in the same table,
 -- giving us much of the flexibility of a no-sql database while still using a relational database.
 CREATE TABLE meta(
     id VARCHAR(21) PRIMARY KEY,   -- nanoid
     memid VARCHAR(24) REFERENCES mem(id),
-    kind VARCHAR(4) NOT NULL,
+    kind VARCHAR(4) NOT NULL CHECK (kind IN ('conv', 'csum', 'doc', 'dsum', 'frag', 'syn', 'xchg')),
     pinned BOOLEAN DEFAULT FALSE,
     priority INT DEFAULT 0,    -- 0 is lowest priority. high priority is unbounded
     role VARCHAR(10),
