45f1d4cb94683c08337fe2b6f9e36f181ff81e59
Jim Lloyd <jim.lloyd@gmail.com>
2024-06-01 10:04:43 -0700

    memento-db: scoreMemsBySimilarityUtil

--- packages/memento-db/src/tests/scoreMemsBySimilarityUtil.ts
+++ packages/memento-db/src/tests/scoreMemsBySimilarityUtil.ts
@@ -2,24 +2,17 @@
 
 import { Command } from 'commander';
 import debug from 'debug';
-import { AddPackageReadmeAgent, AddProjectReadmeAgent, type AddPackageReadmeAgentArgs } from '@memento-ai/readme-agents';
-import { readdir } from 'node:fs/promises';
-import { getProjectRoot } from '@memento-ai/utils';
 import { scoreMemsBySimilarity, type SimilarityMap } from '../scoreMemsBySimilarity';
-import { ProviderNames, isProvider } from '@memento-ai/conversation';
 import { MementoDb } from '../mementoDb';
 import c from 'ansi-colors';
-
-const dlog = debug("update-readmes");
 
 const program = new Command();
 
 program
     .version('0.0.1')
     .description(`A utility to see how scoreMemsBySimilarity performs.`)
-    .option('-p, --provider <provider>', 'The provider to use [anthropic, ollama, openai, ...]')
-    .option('-m, --model <model>', 'The model to use for summarization')
-    .option('-d, --database <dbname>', 'The name of the database to use');
+    .requiredOption('-d, --database <dbname>', 'The name of the database to use')
+    .option('-t, --tokens <int>', 'The total number of tokens to select', '4000');
 
 program.parse(process.argv);
 
@@ -27,22 +20,8 @@ async function main() {
     console.info('Running scoreMemsBySimilarity utility...');
     const options = program.opts();
 
-    const { provider, model, database } = options;
-
-    if (!provider) {
-        console.error(`You must specify an LLM provider (${ProviderNames.join(', ')}`);
-        program.help();
-    }
-
-    if (!isProvider(provider)) {
-        console.error(`The provider ${provider} is not a known provider`);
-        process.exit(1);
-    }
-
-    if (!model) {
-        console.error('You must specify a model supported by the provider');
-        program.help();
-    }
+    let { database, tokens } = options;
+    tokens = parseInt(tokens);
 
     if (!database) {
         console.error('You must provide a database name');
@@ -78,7 +57,7 @@ async function main() {
     for await (const lines of collectLines()) {
         process.stdout.write(`${c.blue('Assistant: ')}`);
         const content: string = lines.join('\n')
-        const result: SimilarityMap = await scoreMemsBySimilarity(db.pool, content, 4000);
+        const result: SimilarityMap = await scoreMemsBySimilarity(db.pool, content, tokens);
 
         for (const key in result) {
             console.log(key, result[key]);
