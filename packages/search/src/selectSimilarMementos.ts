404ca1d24f157a658d58c6d70e4595c6b9cf102f
Jim Lloyd <jim.lloyd@gmail.com>
2024-06-02 22:12:51 -0700

    search: selectSimilarMementos returns combined semantic+keyword search

--- packages/search/src/selectSimilarMementos.ts
+++ packages/search/src/selectSimilarMementos.ts
@@ -1,8 +1,14 @@
+// Path: packages/search/src/selectSimilarMementos.ts
 
 import type { DatabasePool } from 'slonik';
 import type { MementoSearchResult,  MementoSearchArgs } from './mementoSearchTypes';
 import { selectMemsByKeywordSearch } from './selectMemsByKeywordSearch';
 import { selectMemsBySemanticSimilarity } from './selectMemsBySemanticSimilarity';
+import { DOC, SYN, CONV } from '@memento-ai/types';
+import { softmaxNormalize } from './softmaxNormalize';
+import debug from 'debug';
+
+const dlog = debug('search');
 
 // Return a list of mementos that are similar to the given content.
 // The similarity is a combination of keyword search and semantic similarity.
@@ -30,22 +36,22 @@ export async function selectSimilarMementos(dbPool: DatabasePool, args: MementoS
     // The combined score is the arithmetic mean of the two scores.
 
     let result: MementoSearchResult[] = [];
-    let idSet: Set<string> = new Set<string>();
     let kMap: Record<string, MementoSearchResult> = {};
     let sMap: Record<string, MementoSearchResult> = {};
 
     let kScoreSum = keywordSelection.reduce((acc, m) => acc + m.score, 0);
     let sScoreSum = similaritySelection.reduce((acc, m) => acc + m.score, 0);
 
-    console.log(`Keyword score sum: ${kScoreSum}`);
-    console.log(`Similarity score sum: ${sScoreSum}`);
+    dlog(`Keyword score sum: ${kScoreSum}`);
+    dlog(`Similarity score sum: ${sScoreSum}`);
 
     let kTokenSum = keywordSelection.reduce((acc, m) => acc + m.tokens, 0);
     let sTokenSum = similaritySelection.reduce((acc, m) => acc + m.tokens, 0);
 
-    console.log(`Keyword token sum: ${kTokenSum}`);
-    console.log(`Similarity token sum: ${sTokenSum}`);
+    dlog(`Keyword token sum: ${kTokenSum}`);
+    dlog(`Similarity token sum: ${sTokenSum}`);
 
+    let idSet: Set<string> = new Set<string>();
     for (let m of keywordSelection) {
         idSet.add(m.id);
         kMap[m.id] = m;
@@ -63,9 +69,36 @@ export async function selectSimilarMementos(dbPool: DatabasePool, args: MementoS
         result.push({...entry, score});
     }
 
+    let tokens = result.reduce((acc, m) => acc + m.tokens, 0);
+    if (tokens <= maxTokens) {
+        return result;
+    }
+    dlog(`Combined tokens at start: ${tokens}`);
+
+    // We need to trim the result to maxTokens tokens.
+    // The first step should be to remove redundant mementos.
+    // Each of the three kinds 'dsum', 'conv', 'syn' has a `docid` field which is the id of the "source memento",
+    // i.e. these three kinds are "derived" from or contained within a "source" memento.
+
+    result = result.filter((m: MementoSearchResult) => {
+        if (m.kind===DOC || m.kind===SYN || m.kind===CONV) {
+            return !m.docid || !idSet.has(m.docid);
+        } else {
+            return true;
+        }
+    });
+
+    let afterTokens = result.reduce((acc, m) => acc + m.tokens, 0);
+    dlog(`Redundant mementos: removed ${tokens - afterTokens} tokens`);
+    tokens = afterTokens;
+    if (tokens <= maxTokens) {
+        return result;
+    }
+
+    // We stil have too many tokens, so we need to trim the result to maxTokens tokens.
     result.sort((a, b) => b.score - a.score);
 
-    let tokens = 0;
+    tokens = 0;
     for (let i = 0; i < result.length; i++) {
         if (tokens + result[i].tokens> maxTokens) {
             result = result.slice(0, i);
@@ -74,7 +107,8 @@ export async function selectSimilarMementos(dbPool: DatabasePool, args: MementoS
         tokens += result[i].tokens;
     }
 
-    console.log(`Combined tokens: ${tokens}`);
+    dlog(`Combined tokens after filtering: ${tokens}`);
 
-    return result;
+    // Finally, let's renormalize the scores to sum to 1.0.
+    return softmaxNormalize(result, (m) => m.score);
 }
