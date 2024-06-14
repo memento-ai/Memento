01a1044c725ad3dc3b2794962fcd15a57de2567d
Jim Lloyd <jim.lloyd@gmail.com>
2024-06-14 06:00:20 -0700

    memento-agent: revise prompt, add structure&terminology

--- packages/memento-agent/src/prompt-partials/resolutions.ts
+++ packages/memento-agent/src/prompt-partials/resolutions.ts
@@ -7,13 +7,15 @@ const resolutionsText = stripCommonIndent(`
     {{#if resolutions}}
     <resolutions>
     <instructions>
-    Resolutions are statements you have made in the past resolving to affect your behavior in the future.
+    Resolutions are statements you have made in past interactions resolving to affect your behavior in the future.
     These resolutions occur when the user provides feedback to you clarifying your role, responsibilities, or
     expected behavior. When you acknowledge the feedback and resolve to change your behavior in the future,
     your resolution will stored in the database and then presented to you in this section.
     </instructions>
     {{#each resolutions}}
-    <res>{{this}}</res>
+    <res>
+    {{{this}}}
+    </res>
     {{/each}}
     </resolutions>
     {{/if}}
