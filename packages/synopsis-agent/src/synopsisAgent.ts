13916cd665d3745a24d655539938605e29e65330
Jim Lloyd <jim.lloyd@gmail.com>
2024-06-30 10:11:45 -0700

    synopsis-agent: separate config for max_tokens and max_response_tokens

--- packages/synopsis-agent/src/synopsisAgent.ts
+++ packages/synopsis-agent/src/synopsisAgent.ts
@@ -4,7 +4,7 @@ import { Agent, type AgentArgs } from '@memento-ai/agent'
 import type { Config } from '@memento-ai/config'
 import { createConversationFromConfig } from '@memento-ai/conversation'
 import { count_tokens } from '@memento-ai/encoding'
-import { MementoDb, getSynopses } from '@memento-ai/memento-db'
+import { MementoDb, getSynopses, type GetSynopsesArgs } from '@memento-ai/memento-db'
 import { get_last_assistant_message, get_last_user_message } from '@memento-ai/postgres-db'
 import { Message } from '@memento-ai/types'
 import { stripCommonIndent } from '@memento-ai/utils'
@@ -17,16 +17,19 @@ const dlog = debug('synopsis')
 export type SynopsisAgentArgs = AgentArgs & {
     db: MementoDb
     max_tokens: number
+    max_response_tokens: number
 }
 
 export class SynopsisAgent extends Agent {
     private db: MementoDb
     private max_tokens: number
+    private max_response_tokens: number
 
     constructor(args: SynopsisAgentArgs) {
         super(args)
         this.db = args.db
         this.max_tokens = args.max_tokens
+        this.max_response_tokens = args.max_response_tokens
         if (this.max_tokens < 500) {
             throw new Error('synopsis max_tokens must be at least 500')
         }
@@ -45,7 +48,11 @@ export class SynopsisAgent extends Agent {
     }
 
     async getSynopses(): Promise<string[]> {
-        return await getSynopses(this.db.readonlyPool, this.max_tokens)
+        const args: GetSynopsesArgs = {
+            max_response_tokens: this.max_response_tokens,
+            max_tokens: this.max_tokens,
+        }
+        return await getSynopses(this.db.readonlyPool, args)
     }
 
     private async getLatestUserMessage(): Promise<Message> {
@@ -73,6 +80,7 @@ export async function createSynopsisAgent(config: Config, db: MementoDb): Promis
         db,
         conversation,
         max_tokens: config.synopsis_agent.max_tokens,
+        max_response_tokens: config.synopsis_agent.max_response_tokens,
     }
     return new SynopsisAgent(agentArgs)
 }
