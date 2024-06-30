a9b6b320b1fa1f070ffa78f1b78ac70689249be6
Jim Lloyd <jim.lloyd@gmail.com>
2024-06-29 23:11:52 -0700

    search: improvements

--- packages/search/src/normalize.ts
+++ packages/search/src/normalize.ts
@@ -3,8 +3,14 @@
 // This function takes an array of items and a key function that extracts a number (a "score") from each item.
 // It return an array with the score normalized to use the full range of 0..1
 export function linearNormalize<T>(arr: T[], key: (item: T) => number): T[] {
+    if (arr.length == 0) {
+        return arr
+    }
     const scores: number[] = arr.map(key)
     const min: number = Math.min(...scores)
     const max: number = Math.max(...scores)
+    if (min === max) {
+        return arr.map((item) => ({ ...item, score: 0.0 }))
+    }
     return arr.map((item, i) => ({ ...item, score: (scores[i] - min) / (max - min) }))
 }
