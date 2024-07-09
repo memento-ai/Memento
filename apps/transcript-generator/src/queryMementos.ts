2ce0bc80d906930eae7e2871bd564c5886a9b66f
Jim Lloyd <jim.lloyd@gmail.com>
2024-07-09 10:40:08 -0700

    transcript-generator: queryMementos for `conv` mementos only

--- apps/transcript-generator/src/queryMementos.ts
+++ apps/transcript-generator/src/queryMementos.ts
@@ -1,15 +1,6 @@
-import { DatabasePool, sql } from 'slonik';
+import { sql, DatabasePool } from 'slonik';
 import { ConversationMemento } from '@memento-ai/types';
 import { connectReadonlyDatabase } from '@memento-ai/postgres-db';
-
-export async function queryMementos(dbName: string, startDate: Date, endDate: Date): Promise<ConversationMemento[]> {
-  const pool = await connectReadonlyDatabase(dbName);
-  try {
-    return await getConversationByDateRange(pool, startDate, endDate);
-  } finally {
-    await pool.end();
-  }
-}
 
 async function getConversationByDateRange(pool: DatabasePool, startDate: Date, endDate: Date): Promise<ConversationMemento[]> {
   const result = await pool.query(sql.type(ConversationMemento)`
@@ -22,11 +13,20 @@ async function getConversationByDateRange(pool: DatabasePool, startDate: Date, e
     FROM
       memento
     WHERE
-      kind IN ('conv', 'xchg')
+      kind = 'conv'
       AND created_at BETWEEN ${startDate.toISOString()} AND ${endDate.toISOString()}
     ORDER BY
       created_at ASC
   `);
 
   return result.rows.map(r => r);
+}
+
+export async function queryMementos(databaseName: string, startDate: Date, endDate: Date): Promise<ConversationMemento[]> {
+  const pool = await connectReadonlyDatabase(databaseName);
+  try {
+    return await getConversationByDateRange(pool, startDate, endDate);
+  } finally {
+    await pool.end();
+  }
 }
